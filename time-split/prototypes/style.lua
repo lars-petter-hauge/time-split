ac2118c49df6b1807b5389adc32a8fb467c0cdc4
Lars Petter Hauge <larsenhauge@gmail.com>
2022-07-09 21:51:14 +0200

    Apply label padding using prototype

---
+++ time-split/prototypes/style.lua
@@ -0,0 +1,8 @@
+data.raw["gui-style"]["default"]["padded_label"] = {
+    type = "label_style",
+    parent = "label",
+    top_padding = 2,
+    right_padding = 4,
+    bottom_padding = 2,
+    left_padding = 4
+}
